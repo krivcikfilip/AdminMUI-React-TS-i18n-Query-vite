a3f3d64fe37bf6ffb9744665d4c080f50b98bd75
Filip Krivčík <filipkrivcik@gmail.com>
2024-07-25 18:17:38 +0200

    New structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     root: true,
     env: { browser: true, es2020: true },
     extends: [
+<<<<<<< HEAD
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
@@ -13,5 +14,18 @@ module.exports = {
         'simple-import-sort/imports': 'error',
         'mui-path-imports/mui-path-imports': 'error',
         'react/react-in-jsx-scope': 'off',
+=======
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:cypress/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["mui-path-imports", "simple-import-sort", "react-hooks"],
+    rules: {
+        "simple-import-sort/imports": "error",
+        "mui-path-imports/mui-path-imports": "error",
+        "react/react-in-jsx-scope": "off",
+>>>>>>> bdd0d6c (New structure)
     },
 }
